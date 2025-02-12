013a3bbacbaafdcec609233d1bb5b7b8a958766b
Adarsh tiwari <41529730+Shreytiwarijai@users.noreply.github.com>
2025-02-12 11:11:05 +0530

    Create simple_withdrawl.py

---
+++ simple_withdrawl.py
@@ -0,0 +1,15 @@
+cr = 15000
+pin = 5755
+
+dr = int(input("Enter the amount to withdraw: "))
+
+if dr == cr:
+    entered_pin = int(input("Enter your pin: "))
+    if entered_pin == pin:
+        print("Withdrawal successful")
+        print("You're remaining balance is: ", cr - dr)
+    else:
+        print("You've entered the wrong pin")
+else:
+    print("Invalid withdrawal amount")
+
